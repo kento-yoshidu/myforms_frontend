fec9f45e3411bf61d717503b6e0e5046fa2c35a4
kento <kento@toriwatari.bird>
2023-03-12 20:47:44 +0900

    ✨ Bugfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
 
 import "./styles/style.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </>
   )
 }
