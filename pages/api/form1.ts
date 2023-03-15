1df20d254d4aa7e1fba7b133474ad93c6f8bba1b
kento <kento@toriwatari.bird>
2023-03-15 20:06:53 +0900

    ✨ Update

--- pages/api/form1.ts
+++ pages/api/form1.ts
@@ -5,9 +5,12 @@ export default function handler(
   res: NextApiResponse
 ) {
   try {
-    console.log(req.body)
-
-    res.status(200).json({ data: "test" })
+    res
+      .status(200)
+      .json({
+        username: req.body.username,
+        email: req.body.email
+      })
   } catch(e) {
     res.status(500)
   }
