537df6f0431aa489a54d9ab1c41c3cc367b0cfb5
kento <kento@toriwatari.bird>
2023-03-25 13:46:23 +0900

    ✨ Update

--- __tests__/form1.test.tsx
+++ __tests__/form1.test.tsx
@@ -24,16 +24,34 @@ afterEach(() => {
 afterAll(() => server.close())
 
 describe("Form1", () => {
-  it("Rendering Form1", () => {
+  it("Rendering Form1.", () => {
     render(<Form1 />)
     expect(screen.getByTestId("form-title")).toHaveTextContent(/^Form1$/)
   })
 
-  it("送信テスト", async () => {
+  it("First rendered, nothing is displayed in the result-area", () => {
+    render(<Form1 />)
+    expect(screen.queryByTestId("result-area")).toBeNull()
+  })
+
+  it("First rendered, button should be disabled.", () => {
+    render(<Form1 />)
+    expect(screen.getByRole("button")).toBeDisabled()
+  })
+
+  it("Input value, button should be not disabled.", async () => {
+    render(<Form1 />)
+    await userEvent.type(screen.getByTestId("name"), "kento")
+    expect(screen.getByRole("button")).not.toBeDisabled()
+  })
+
+  /*
+  it("First rendered, button should be disabled.", async () => {
     render(<Form1 />)
     const nameForm = screen.getByTestId("name") as HTMLInputElement
     await userEvent.type(nameForm, "kento")
     await userEvent.click(screen.getByTestId("submit"))
     screen.debug()
   })
+  */
 })
---
+++ styles/pageTitle.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "wrapper": string;
+  readonly "pageTitle": string;
+};
+export = styles;
+
