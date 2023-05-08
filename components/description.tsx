254373d0f7c9c0cb5f9b03d1606da1deccf86e6c
kento <kento@toriwatari.bird>
2023-05-08 22:04:33 +0900

    ✨ Descriptionをdetails要素で作り直し

--- components/description.tsx
+++ components/description.tsx
@@ -1,57 +1,21 @@
-import { CSSProperties, ReactNode, useRef, useState } from "react"
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCircleChevronDown } from "@fortawesome/free-solid-svg-icons"
+import { ReactNode, useRef, useState } from "react"
 
 import styles from "../styles/description.module.css"
 
 type Props = {
-  children: ReactNode,
   heading?: string
+  children: ReactNode,
 }
 
-const Description = ({ children, heading }: Props) => {
-  const [textIsOpen, setTextIsOpen] = useState(false)
-
-  const toggleText = () => {
-    setTextIsOpen((prev) => !prev)
-  }
-
-  const refText = useRef<HTMLDivElement>(null)
-
+const Description = ({ heading, children }: Props) => {
   return (
-    <div
-      className={textIsOpen ? styles.open : styles.close}
-    >
-      <h3 className={styles.heading}>
-        <button onClick={toggleText}>
-          {heading
-            ? heading
-            : "こぼれ話"
-          }
-          <FontAwesomeIcon
-            /* @ts-ignore */
-            icon={faCircleChevronDown}
-            className={styles.icon}
-          />
-        </button>
-      </h3>
+    <details className={styles.details}>
+      <summary className={styles.summary}>{ heading ? heading : "こぼれ話" }</summary>
 
-      <div
-        aria-hidden={textIsOpen ? "false" : "true"}
-        className={styles.text}
-        ref={refText}
-        style={{
-          "--text-height": refText.current
-            ?  `${refText.current?.scrollHeight}px`
-            : "0px"
-        } as CSSProperties}
-      >
-        <div className={styles.textInner}>
-          {children}
-        </div>
+      <div className={styles.text}>
+        {children}
       </div>
-    </div>
+    </details>
   )
 }
 
