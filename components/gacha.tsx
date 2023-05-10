03fb27c1d56b91a35f7886eb5cdb5bfa1c0172db
kento <kento@toriwatari.bird>
2023-05-10 20:54:31 +0900

    ✨ #132 ガチャ基本機能完成

--- components/gacha.tsx
+++ components/gacha.tsx
@@ -1,18 +1,54 @@
 import { useState } from "react"
+import Link from "next/link"
+
+import Loading from "./loading"
 
 import styles from "../styles/gacha.module.css"
-import Link from "next/link"
 
 const Gacha = () => {
   const [form, setForm] = useState<number | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = () => {
+    new Promise((resolve) => {
+      setIsLoading(true)
+      setForm(Math.ceil(Math.random() * 5))
+
+      setTimeout(() => {
+        resolve("")
+      }, 1500)
+    })
+    .then(() => {
+      setIsLoading(false)
+    })
+  }
 
   return (
     <div className={styles.wrapper}>
-      <button className={styles.button}>ガチャを回す</button>
+      <button
+        className={styles.button}
+        onClick={handleClick}
+      >
+        ガチャを回す
+      </button>
 
-      <div className={styles.result}>
-        <Link href="form1">Form1</Link>
-      </div>
+      {isLoading
+        ? (
+          <Loading />
+        )
+        : (
+          <div
+            className={styles.result}
+          >
+            {form
+              ? (
+                <Link href={`form${form}`}>Form{form}</Link>
+              )
+              : (
+                <p>ガチャを回してね</p>
+              )}
+          </div>
+        )}
     </div>
   )
 }
