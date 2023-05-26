f845ad43aeb57fbca14a58ebcad155a1f7481a78
kento <kento@toriwatari.bird>
2023-05-26 21:30:03 +0900

    ✨ Update

--- pages/form6/index.tsx
+++ pages/form6/index.tsx
@@ -126,7 +126,7 @@ const Form6 = () => {
 
         <Description>
           <p>Form1、Form2と同じく、入力した半角小文字アルファベットを大文字にして表示するフォームです。それらは半角小文字アルファベット以外（ここでは「変換不可文字」と呼びます）も入力し送信可能でしたが、今回は変換不可文字が混じっている時はそもそもフォームを送信できない仕様にしています。</p>
-          <p>また、変換不可文字を入力すると、その時点で「<span style={{ "color": "red" }}>半角小文字アルファベットと半角スペースのみ入力可能です。</span>」と表示され、さらにテキストボックスのborderが赤くなります。これが本当のリアルタイムバリデーションですね。</p>
+          <p>また、変換不可文字を入力すると、その時点で「<span style={{ "color": "rgb(220, 50, 50)" }}>半角小文字アルファベットと半角スペースのみ入力可能です。</span>」と表示され、さらにテキストボックスのborderが赤くなります。これが本当のリアルタイムバリデーションですね。</p>
 
           <p>テストコードは<a href="https://github.com/kento-yoshidu/MyForms/blob/main/__tests__/form6.test.tsx">こちら</a>です。テストに関して特筆すべきことはしていないので省略します。</p>
 
@@ -135,7 +135,7 @@ const Form6 = () => {
           <p>フォームに入力された値が変換不可文字を含んでいる場合、useStateを用いて<em>aria-invalid属性</em>の値をtrueにするようにコードを書いています。</p>
           <p>aria-invalid属性とは何でしょうか？私も詳しくは説明できないので仕様書を読んでほしいのですが（<a href="https://www.w3.org/TR/wai-aria-1.1/#aria-invalid" target="_blink">WAI-ARIAの仕様書</a>）、入力された値が不正な場合は値をtrueにして、その旨をユーザーや支援技術に知らせるaria属性である、と読み取りました。</p>
 
-          <p>そしてCSSでは、このaria-invalid属性の値に応じてスタイルを返るようにしています（コードは<a href="https://github.com/kento-yoshidu/MyForms/blob/053eda05aa5c9b2e9fb51ae4a16c4ee8a16e9435/pages/form1/style.module.css#L91-L95" target="_blink">こちら</a>）。ついつい「validate-error」などといったCSSクラスを付与してスタイルを切り替えてしまいそうな所ですが、よりセマンティックになるようにCSSを書いています。</p>
+          <p>そしてCSSでは、このaria-invalid属性の値に応じてスタイルを返るようにしています（コードは<a href="https://github.com/kento-yoshidu/MyForms/blob/053eda05aa5c9b2e9fb51ae4a16c4ee8a16e9435/pages/form1/style.module.css#L91-L95" target="_blink">こちら</a>）。WAI-ARIAを勉強するまでの私なら「.validate-error」などといったCSSクラスを付与してスタイルを切り替えてしまっていた所ですが、よりセマンティックになるようにCSSを書いています。</p>
         </Description>
 
         <Description heading="更新履歴">
